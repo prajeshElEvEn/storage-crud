338777fc8b3d3bb70d86bbd2fa4258e2e3c840c1
prajeshElEvEn <prajesh.eleven118@gmail.com>
2022-05-28 08:06:02 +0530

    - added storage - added .env - added published link - updated README

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { storage } from './assets/config';
 
 function App() {
+  const [image, setImage] = useState(null)
+  const [imageUrl, setImageUrl] = useState('')
+
+  const uploadFile = async () => {
+    if (image) {
+      const imageRef = ref(storage, `images/wallpapers/${image.name}`)
+      await uploadBytes(imageRef, image).then(() => {
+        getDownloadURL(imageRef).then((url) => {
+          setImageUrl(url)
+        })
+      })
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input
+          type='file'
+          onChange={(e) => {
+            setImage(e.target.files[0])
+          }}
+        />
+        <input
+          type='submit'
+          value='Upload'
+          onClick={() => {
+            uploadFile()
+          }}
+        />
+      </div>
+      <div>
+        {imageUrl}
+      </div>
     </div>
   );
 }
---
+++ src/assets/config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+import { getDatabase } from "@firebase/database"
+import { getStorage } from 'firebase/storage'
+import { getAnalytics } from "firebase/analytics"
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+}
+
+const app = initializeApp(firebaseConfig)
+const db = getDatabase(app)
+const storage = getStorage(app)
+const analytics = getAnalytics(app)
+
+export { db, storage, analytics }
